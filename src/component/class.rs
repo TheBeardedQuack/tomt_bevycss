030a0976a0caafdc401fb749b0f81a0e58a3e5e6
TheBeardedQuack <TheBeardedQuack@gmail.com>
2023-04-03 00:13:05 +0100

    Moved Class into its own module

---
+++ src/component/class.rs
@@ -0,0 +1,46 @@
+use super::MatchSelectorElement;
+use std::borrow::Cow;
+use bevy::prelude::{
+    Component, Handle, Name,
+    Deref,
+    Reflect, ReflectComponent,
+};
+
+/// Sets the entities class to be matched by selectors in on`css`.
+///
+/// The behavior mimics CSS so a single class name can given or a list separated by spaces.
+///
+/// # Examples
+///
+/// ```
+/// # use bevy::prelude::*;
+/// # use tomt_bevycss::prelude::*;
+/// fn system(mut commands: Commands) {
+///     // This entity can be selected by either ".yellow-button", ".enabled"
+///     // or even ".yellow-button.enabled"
+///     commands.spawn(Class::new("yellow-button enabled"));
+/// }
+/// ```
+#[derive(Debug, Reflect, Component, Default, Clone, Deref)]
+#[reflect(Component)]
+pub struct Class(Cow<'static, str>);
+
+impl Class {
+    /// Creates a new [`Class`] with the given class names.
+    ///
+    /// Multiple class names can be used separated by spaces.
+    pub fn new(class: impl Into<Cow<'static, str>>) -> Self {
+        Self(class.into())
+    }
+
+    /// Checks if any of this class names matches the given class name
+    fn matches(&self, class: &str) -> bool {
+        self.0.split_ascii_whitespace().any(|c| c == class)
+    }
+}
+
+impl MatchSelectorElement for Class {
+    fn matches(&self, element: &str) -> bool {
+        self.matches(element)
+    }
+}
--- src/component/mod.rs
+++ src/component/mod.rs
@@ -1,41 +1,15 @@
-use std::borrow::Cow;
+mod class;
+pub use class::*;
 
-use bevy::prelude::{Component, Deref, Handle, Name, Reflect, ReflectComponent};
+use bevy::prelude::{
+    Component,
+    Handle,
+    Name,
+    Reflect,
+    ReflectComponent
+};
 
 use crate::StyleSheetAsset;
-
-/// Sets the entities class to be matched by selectors in on`css`.
-///
-/// The behavior mimics CSS so a single class name can given or a list separated by spaces.
-///
-/// # Examples
-///
-/// ```
-/// # use bevy::prelude::*;
-/// # use tomt_bevycss::prelude::*;
-/// fn system(mut commands: Commands) {
-///     // This entity can be selected by either ".yellow-button", ".enabled"
-///     // or even ".yellow-button.enabled"
-///     commands.spawn(Class::new("yellow-button enabled"));
-/// }
-/// ```
-#[derive(Debug, Reflect, Component, Default, Clone, Deref)]
-#[reflect(Component)]
-pub struct Class(Cow<'static, str>);
-
-impl Class {
-    /// Creates a new [`Class`] with the given class names.
-    ///
-    /// Multiple class names can be used separated by spaces.
-    pub fn new(class: impl Into<Cow<'static, str>>) -> Self {
-        Self(class.into())
-    }
-
-    /// Checks if any of this class names matches the given class name
-    fn matches(&self, class: &str) -> bool {
-        self.0.split_ascii_whitespace().any(|c| c == class)
-    }
-}
 
 /// Applies a [`StyleSheetAsset`] on the entity which has this component.
 ///
@@ -93,12 +67,6 @@ impl PartialEq for StyleSheet {
 /// Convenience trait which matches matches a component against a named element selector.
 pub(crate) trait MatchSelectorElement {
     fn matches(&self, element: &str) -> bool;
-}
-
-impl MatchSelectorElement for Class {
-    fn matches(&self, element: &str) -> bool {
-        self.matches(element)
-    }
 }
 
 impl MatchSelectorElement for Name {
