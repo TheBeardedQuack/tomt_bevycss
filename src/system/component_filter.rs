8bb755cec58313334c0593bff10cc23d6abe1027
TheBeardedQuack <TheBeardedQuack@gmail.com>
2023-04-03 23:47:05 +0100

    Moved default implementation of ComponentFilter into component_filter.rs

--- src/system/component_filter.rs
+++ src/system/component_filter.rs
@@ -1,8 +1,18 @@
 use smallvec::SmallVec;
-use bevy::prelude::{
-    Entity, World,
+use bevy::{
+    prelude::{
+        Entity, Component, World, 
+        Query, With,
+    },
+    ecs::system::SystemState,
 };
 
 pub(crate) trait ComponentFilter {
     fn filter(&mut self, world: &World) -> SmallVec<[Entity; 8]>;
+}
+
+impl<'w, 's, T: Component> ComponentFilter for SystemState<Query<'w, 's, Entity, With<T>>> {
+    fn filter(&mut self, world: &World) -> SmallVec<[Entity; 8]> {
+        self.get(world).iter().collect()
+    }
 }
--- src/system/mod.rs
+++ src/system/mod.rs
@@ -24,12 +24,6 @@ use crate::{
     StyleSheetAsset,
 };
 
-impl<'w, 's, T: Component> ComponentFilter for SystemState<Query<'w, 's, Entity, With<T>>> {
-    fn filter(&mut self, world: &World) -> SmallVec<[Entity; 8]> {
-        self.get(world).iter().collect()
-    }
-}
-
 #[derive(Deref, DerefMut, Resource)]
 pub(crate) struct PrepareParams(SystemState<CssQueryParam<'static, 'static>>);
 
